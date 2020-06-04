d918c6d2b0e2811ba7b7b488554da5f8518f958e
Mohammed Al-Khalifah <mohak2009@hotmail.com>
2020-06-04 17:34:07 +0300

    finishing everything and preparing to deploy

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register'
 
 
 
 const app = new Clarifai.App({
- apiKey: '48bc3d2df4dc4525851b63c89dcbd593'
+ apiKey: 'd217f992ade146028bc4a1412d15eecc'
 });
 
 const particlesOptions = {
@@ -32,8 +34,41 @@ class App extends Component {
     this.state = {
       input : '',
       imageUrl : '',
-      box: {}
+      box: {},
+      route:'signIn',
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        password: '',
+        entries: 0,
+        joined: new Date()
+      }
     }
+  }
+
+  // this.setState(user:{
+  //   id: user.id,
+  //   name: user.name,
+  //   email: user.email,
+  //   password: user.password,
+  //   entries: user.entries,
+  //   joined: user.joined
+
+  // })
+
+  loadUser = (data)=>{ 
+    this.setState({user:{
+    id: data.id,
+    name: data.name,
+    email: data.email,
+    password: data.password,
+    entries: data.entries,
+    joined: data.joined
+
+    }})
+    
   }
 
 
@@ -54,10 +89,6 @@ class App extends Component {
 
   }
 
-  sayHi = ()=>{
-    console.log("data");
-  }
-
   
   displayFaceBox = (box) => {
     this.setState({box: box});
@@ -74,34 +105,69 @@ class App extends Component {
     this.setState({imageUrl: this.state.input});
    // console.log(this.imageUrl);
 
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-    function(response) {
-      console.log(response);
-      const data = response;
-      console.log(data);
-      this.displayFaceBox(this.calculateFaceLocation(data)); 
-    },
-    function(err) {
-       console.log(err);
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+       this.state.input)
+    .then(response=>{
+
+      if(response){
+        fetch('http://localhost:3000/image',{
+          method: 'post',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        }).then(response=>response.json())
+          .then(count=>{
+            this.setState(Object.assign(this.state.user, { entries: count}))
+          })
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+    .catch(err=>console.log(err));
+     
+  }
+
+  onRouteChange = (route)=>{
+    if (route ==='signout'){
+      this.setState({isSignedIn: false})
+    } else if(route ==='home'){
+      this.setState({isSignedIn: true})
     }
-  );
+    this.setState({route: route})
   }
   
 
 
   render(){
+
+    const {isSignedIn, route, imageUrl, box} = this.state
     return (
+
+     
       <div className="App">
 
         <Particles className = 'particles'
                 params={particlesOptions} />
 
-        <Navigation/>
-        <Rank/>
-        <Logo/>
-        <ImageLinkForm onInputChange = {this.onInputChange } onButtonSubmit = {this.onButtonSubmit}/>
-        <FaceRecognition box = {this.state.box} imageUrl = {this.state.imageUrl}/>
-
+        <Navigation isSignedIn = {isSignedIn} onRouteChange = {this.onRouteChange}/>
+        { 
+        route === 'home'?
+        <div>
+            <Rank name = {this.state.user.name} entries = {this.state.user.entries}/>
+            <Logo/>
+            <ImageLinkForm onInputChange = {this.onInputChange } onButtonSubmit = {this.onButtonSubmit}/>
+            <FaceRecognition box = {box} imageUrl = {imageUrl}/>
+          </div>
+        
+        : (this.state.route === 'signIn'? 
+        <SignIn loadUser={this.loadUser}  onRouteChange = {this.onRouteChange}/> :
+        <Register loadUser={this.loadUser} onRouteChange = {this.onRouteChange} />
+          )
+          
+       
+        }
       </div>
     );
   }
